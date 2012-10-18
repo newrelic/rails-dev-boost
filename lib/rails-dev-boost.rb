4974bee654a2256df2a8dd2bdf960e91f5108ef0
Tom Lee <tom@newrelic.com>
2012-10-18 15:07:36 -0700

    Enable or disable rails-dev-boost with an environment variable.

--- lib/rails-dev-boost.rb
+++ lib/rails-dev-boost.rb
@@ -1,2 +1,11 @@
-require 'rails_development_boost' # require the proper module in case somebody forgets to tell bundler to :require => 'rails_development_boost'
-RailsDevelopmentBoost.init!
+#
+# Only enable rails-dev-boost if:
+#
+#   * FORCE_RELOAD environment variable is unset; or
+#   * FORCE_RELOAD looks like some sort of false value
+#
+if ENV["FORCE_RELOAD"].blank? || !["yes", "true", "1", "on"].include?(ENV["FORCE_RELOAD"])
+  require 'rails_development_boost' # require the proper module in case somebody forgets to tell bundler to :require => 'rails_development_boost'
+  RailsDevelopmentBoost.init!
+end
+
